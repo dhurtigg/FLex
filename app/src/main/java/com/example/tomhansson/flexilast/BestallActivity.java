e3153d305a1df6a810545a1005bf147ff0c8034e
dhurtigg <daniel.hurtig@hotmail.com>
2016-05-19 17:06:33 +0200

    magi

--- app/src/main/java/com/example/tomhansson/flexilast/BestallActivity.java
+++ app/src/main/java/com/example/tomhansson/flexilast/BestallActivity.java
@@ -1,5 +1,6 @@
 package com.example.tomhansson.flexilast;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -7,6 +8,13 @@ import android.support.v7.app.AppCompatActivity;
  * Created by Daniel on 2016-05-19.
  */
 public class BestallActivity extends AppCompatActivity {
+
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent i = new Intent(this, PlattActivity.class);
+        startActivity(i);
+        this.finish();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
